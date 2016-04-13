ea14caf5968d9f9cb527331ed1fc413de5fea41e
Arun Sittampalam <arun@sittampalam.ch>
2016-04-13 16:58:09 +0200

    Routed root to home#index for now. We should probably root it directly to the search form after it's implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'home#index', locale: :de
   scope "(:locale)", locale: [/#{I18n.available_locales.join("|")}/], defaults: {locale: :de} do
     get 'home/index'
   end
