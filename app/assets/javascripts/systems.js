ccb12438e2db827921a778ccf409165ffdbbdf91
Tim Peter <tim.peter@eonum.ch>
2016-04-19 13:32:27 +0200

    Version selection dropdown behaviour.

--- app/assets/javascripts/systems.js
+++ app/assets/javascripts/systems.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$( function() {
+    var change_system = function() {
+        var system_url = $('#version-select').find(":selected").val();
+        window.location.href = system_url;
+    }
+
+    $(document).on('change', '#version-select', change_system);
+});
