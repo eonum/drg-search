c9531dc58c44df34a8b90a2ca9e503c46df28c0f
Tim Peter <tim.peter@eonum.ch>
2016-04-22 10:15:54 +0200

    Link codes search behaviour with UI.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -7,8 +7,26 @@ $( function() {
         searchUrl = $('#hospital_search').data('search-url');
         $.get(searchUrl, {term: searchTerm, limit: 5, format: 'json'})
             .done(function (data) {
-                $('#hospitalSearchResults').html(data);
+                $('#hospitalSearchResults').html(data[0]['text']);
                 $('.nav-tabs a[href="#hospitalSearchResults"]').tab('show');
+            });
+    });
+
+    $('#number_search').keydown(function () {
+        searchTerm = $('#number_search').val();
+        searchUrl = $('#number_search').data('search-url');
+        $.get(searchUrl, {term: searchTerm, limit: 5, format: 'json', level: 'drg'})
+            .done(function (data) {
+                $('#drgSearchResults').html(data[0]['text']);
+                $('.nav-tabs a[href="#drgSearchResults"]').tab('show');
+            });
+        $.get(searchUrl, {term: searchTerm, limit: 5, format: 'json', level: 'mdc'})
+            .done(function (data) {
+                $('#mdcSearchResults').html(data[0]['text']);
+            });
+        $.get(searchUrl, {term: searchTerm, limit: 5, format: 'json', level: 'adrg'})
+            .done(function (data) {
+                $('#adrgSearchResults').html(data[0]['text']);
             });
     });
 });
