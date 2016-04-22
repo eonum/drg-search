cff4ef8ab93ceebe3173b0b7da826a32f19c3a22
Tim Peter <tim.peter@eonum.ch>
2016-04-22 09:58:26 +0200

    Link hospital search behaviour to UI.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$( function() {
+    $('#hospital_search').keydown(function () {
+        searchTerm = $('#hospital_search').val();
+        searchUrl = $('#hospital_search').data('search-url');
+        $.get(searchUrl, {term: searchTerm, limit: 5, format: 'json'})
+            .done(function (data) {
+                $('#hospitalSearchResults').html(data);
+                $('.nav-tabs a[href="#hospitalSearchResults"]').tab('show');
+            });
+    });
+});
