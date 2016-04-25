01bdb295d6d4f938fb98614af9057b354aa02d87
Tim Peter <tim.peter@eonum.ch>
2016-04-25 11:52:57 +0200

    Data from base year and additional years.

--- app/helpers/systems_helper.rb
+++ app/helpers/systems_helper.rb
@@ -1,5 +1,9 @@
 module SystemsHelper
   def data_from system
-    return I18n.t('data_from') + ' ' + system.years.join(', ')
+    additional_years = system.years - [system.base_year]
+    additional_data = ''
+    additional_data = I18n.t('additional_data_singular') + ' ' + additional_years[0].to_s if additional_years.length == 1
+    additional_data = I18n.t('additional_data_plural') + ' ' + additional_years.join(', ') if additional_years.length > 1
+    return I18n.t('data_from') + ' ' + system.base_year.to_s + ' ' + additional_data
   end
 end
