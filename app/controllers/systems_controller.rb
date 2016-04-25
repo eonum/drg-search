b368a0a3e1927b7875409e494376d485e3c847fd
Tim Peter <tim.peter@eonum.ch>
2016-04-25 13:41:34 +0200

    Store selected hospitals and codes in hidden inputs.

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,6 +1,13 @@
 class SystemsController < ApplicationController
   def show
     @system = System.find(params[:id])
+    params_hospitals = params[:hospitals] == nil ? [] : params[:hospitals].split(',')
+    @hospitals = Hospital.where(id: params_hospitals)
+    param_codes = params[:codes] == nil ? [] : params[:codes].split(',')
+    @codes = Mdc.where(code: param_codes, version: @system.version)
+    @codes += Drg.where(code: param_codes, version: @system.version)
+    @codes += Adrg.where(code: param_codes, version: @system.version)
+    @codes += Partition.where(code: param_codes, version: @system.version)
   end
 
   def index
