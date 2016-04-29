a658a89d10b2c07c3d4cca28d0a3674bd4fcf961
Arun Sittampalam <arun@sittampalam.ch>
2016-04-29 18:00:18 +0200

    Working on keeping hospital and code params when switching locales.

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -16,13 +16,13 @@ class SystemsController < ApplicationController
   private
     def set_variables
       @system = System.find(params[:id])
-      params_hospitals = params[:hospitals] == nil ? [] : params[:hospitals].split(',')
-      @hospitals = Hospital.where(id: params_hospitals)
-      param_codes = params[:codes] == nil ? [] : params[:codes].split(',')
-      @codes = Mdc.where(code: param_codes, version: @system.version)
-      @codes += Drg.where(code: param_codes, version: @system.version)
-      @codes += Adrg.where(code: param_codes, version: @system.version)
-      @codes += Partition.where(code: param_codes, version: @system.version)
+      @params_hospitals = params[:hospitals] == nil ? [] : params[:hospitals].split(',')
+      @hospitals = Hospital.where(id: @params_hospitals)
+      @param_codes = params[:codes] == nil ? [] : params[:codes].split(',')
+      @codes = Mdc.where(code: @param_codes, version: @system.version)
+      @codes += Drg.where(code: @param_codes, version: @system.version)
+      @codes += Adrg.where(code: @param_codes, version: @system.version)
+      @codes += Partition.where(code: @param_codes, version: @system.version)
 
       hop_ids = @hospitals.map {|h| h.hospital_id }
       codes = @codes.map {|c| c.code }
