6bd69950c3d34da7aa05226562878b8004cbc893
Tim Peter <tim.peter@eonum.ch>
2016-05-02 19:18:45 +0200

    Different naming of systems.
    Emphasize year instead of SwissDRG version as this is probably better understood by the average user.

--- app/helpers/systems_helper.rb
+++ app/helpers/systems_helper.rb
@@ -4,7 +4,8 @@ module SystemsHelper
     additional_data = ''
     additional_data = I18n.t('additional_data_singular') + ' ' + additional_years[0].to_s if additional_years.length == 1
     additional_data = I18n.t('additional_data_plural') + ' ' + additional_years.join(', ') if additional_years.length > 1
-    return I18n.t('data_from') + ' ' + system.base_year.to_s + ' ' + additional_data
+    application = I18n.t('application_year') + system.application_year.to_s + ' (' + system.text(locale) + '). '
+    return application + I18n.t('data_from') + ' ' + system.base_year.to_s + ' ' + additional_data
   end
 
 
