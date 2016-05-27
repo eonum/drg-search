bc5fd098d22c1f0469da0274e061bba82aa0e0e3
Tim Peter <tim.peter@eonum.ch>
2016-05-27 14:12:50 +0200

    Remove home page and instead redirect to current system.
    Remove info and about sites and replace with documentation.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   scope '/:locale', :locale => /de|fr|it/, :format => /json|html/ do
     get '', to: 'home#index', as: 'home'
     get 'contact', to: 'home#contact'
-    get 'about', to: 'home#about'
-    get 'help', to: 'home#help'
+    get 'documentation', to: 'home#documentation'
 
 
     resources :systems,  only: ['index', 'show'] do
