f8a7f98d67ef314df2dbc6c7ca67651dcad4c0a3
Tim Peter <tim.peter@eonum.ch>
2016-05-30 14:27:05 +0200

    Add MeSH synonyms.
    Not yet working.

--- app/models/adrg.rb
+++ app/models/adrg.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Adrg < ActiveRecord::Base
   belongs_to :mdc
   belongs_to :partition
@@ -5,7 +7,8 @@ class Adrg < ActiveRecord::Base
 
   include MultiLanguageText
 
-  searchkick word_middle: [:text_de, :text_fr, :text_it], callbacks: false, language: 'german'
+  searchkick word_middle: [:text_de, :text_fr, :text_it], callbacks: false, language: 'german',
+             synonyms: -> { CSV.read('data/mesh_2016/synonyms.csv', {col_sep: ';'}) }
 
   def code_display
     return code
--- app/models/drg.rb
+++ app/models/drg.rb
@@ -5,7 +5,8 @@ class Drg < ActiveRecord::Base
 
   include MultiLanguageText
 
-  searchkick word_middle: [:text_de, :text_fr, :text_it], callbacks: false, language: 'german'
+  searchkick word_middle: [:text_de, :text_fr, :text_it], callbacks: false, language: 'german',
+             synonyms: -> { CSV.read('data/mesh_2016/synonyms.csv', {col_sep: ';'}) }
 
   def code_display
     return code
