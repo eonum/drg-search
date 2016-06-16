c7cf9ff4c36b1139bc364cca9bb27352c90684e2
Tim Peter <tim.peter@eonum.ch>
2016-06-16 14:59:10 +0200

    Add some custom synonyms for hospitals.

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -3,7 +3,9 @@
 class Hospital < ActiveRecord::Base
   has_many :num_cases
 
-  searchkick word_middle: [:name, :address], language: 'german', callbacks: false
+  searchkick word_middle: [:name, :address], language: 'german', callbacks: false,
+             synonyms: [['freiburg', 'fribourg'], ['centre hospitalier', 'spitalzentrum'], ['Biel', 'Bienne'], ['neuenburg', 'Neuchâtel', 'Neuchatel'],
+             ['Hôpital de l’île', 'Inselspital'], ['Genf', 'Geneve', 'Genève'], ['Bern', 'Berne', 'Berna'], ['Geburtshaus', 'Maison de naissance']]
   # hospital_id is only unique within one year
   # id is unique over all hospital objects
 end
