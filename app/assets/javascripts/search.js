c5f451befb799a5bd10a3e3a62db58f5f7ed2dcb
Tim Peter <tim.peter@eonum.ch>
2016-06-21 19:29:24 +0200

    Optimize and clean up search.js.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -22,7 +22,7 @@ $( function() {
         $(element).val(codes.join(','));
         $('#hintSuccess').prop('hidden', true);
         updateComparison();
-    }
+    };
 
     var addRemoval = function(){
         $('.removeCode').click(function(){
@@ -32,10 +32,12 @@ $( function() {
         $('.removeHospital').click(function(){
             deleteItem($(this).data('hospital-id'), '#hospitals');
         });
-    }
+    };
 
     var addSorting = function() {
-        $( ".sortableHospitals" ).sortable({
+        var sortableHospitals = $( ".sortableHospitals" );
+        var sortableCodes = $( ".sortableCodes" );
+        sortableHospitals.sortable({
             stop: function( event, ui ) {
                 var hospitals = [];
                 $( ".sortableHospitals").children('.selection').each(function () {
@@ -45,8 +47,8 @@ $( function() {
                 updateComparison();
             }
         });
-        $( ".sortableHospitals" ).disableSelection();
-        $( ".sortableCodes" ).sortable({
+        sortableHospitals.disableSelection();
+        sortableCodes.sortable({
             stop: function( event, ui ) {
                 var codes = [];
                 $( ".sortableCodes").children('.selection').each(function () {
@@ -56,18 +58,19 @@ $( function() {
                 updateComparison();
             }
         });
-        $( ".sortableCodes" ).disableSelection();
-    }
+        sortableCodes.disableSelection();
+    };
 
     var addHierarchicalSelection = function() {
         $('.hierarchicalCodeSelection').click(function(){
             var newCode = $(this).data('newcode');
             var oldCode = $(this).data('oldcode');
-            var codes = $('#codes').val($('#codes').val().replace(oldCode, newCode));
+            var codesHidden = $('#codes');
+            var codes = codesHidden.val(codesHidden.val().replace(oldCode, newCode));
             updateComparison();
             return false;
         });
-    }
+    };
 
     function getActiveTab() {
         var activeTab = $("ul#resultsTabs li.active a")[0];
@@ -81,23 +84,26 @@ $( function() {
      */
     var setURL = function(){
         var path = purl().data.attr.path;
-        var hospitals = $('#hospitals').val();
-        var codes = $('#codes').val();
-        var searchTermHospital = $('#hospital_search').val();
-        var searchTermCodes = $('#codes_search').val();
+        if(path.indexOf('system') < 0)
+            return;
+
+        var hospitals = $("#hospitals").val();
+        var codes = $("#codes").val();
+        var searchTermHospital = $("#hospital_search").val();
+        var searchTermCodes = $("#codes_search").val();
 
         var params = '?codes=' + codes + '&hospitals=' + hospitals;
         params += '&activeTab=' + getActiveTab() + '&hospitalSearch=' + searchTermHospital;
 
         if("replaceState" in window.history)
             window.history.replaceState({}, 'DRG comparison', path + params);
-        $('.lang-selection').each(function() {
+        $(".lang-selection").each(function() {
             $(this).attr('href', $(this).attr('data') + params);
         });
-        $('.version-selection').each(function() {
+        $(".version-selection").each(function() {
             $(this).attr('value', $(this).data('url') + params);
         });
-    }
+    };
 
     /**
      * Get the results tab section based on the selected hospitals and codes.
@@ -108,85 +114,88 @@ $( function() {
         var url = $('#compareUrl').val();
         $.get(url, {codes: codes, hospitals: hospitals, activeTab: getActiveTab()})
             .done(function (data) {
-                $('#comparison-resultsbox').html(data);
+                $("#comparison-resultsbox").html(data);
                 addRemoval();
                 addSorting();
                 addHierarchicalSelection();
-                $('#resultsTabs li a').on('shown.bs.tab', function (e) { setURL() });
+                $("#resultsTabs li a").on('shown.bs.tab', function (e) { setURL() });
             });
         setURL();
 
         /* first reenable all. */
-        $('.searchResultEntry').removeClass('alreadySelected');
-        $('.searchResultEntry').find( ".btn" ).prop("disabled",false);
-        $('.searchResultEntry').find( ".btn" ).prop('title', I18n.t('take_over'));
+        var searchResulEntry = $(".searchResultEntry");
+        searchResulEntry.removeClass('alreadySelected');
+        searchResulEntry.find( ".btn" ).prop("disabled",false);
+        searchResulEntry.find( ".btn" ).prop('title', I18n.t('take_over'));
         /* then disable already selected. */
         disableAlreadySelected();
-    }
+    };
 
     var assembleArray = function(item, list){
         var temp = [];
         for (var i = 0; i < list.length; i++){
-            var h = list[i]
+            var h = list[i];
             if(h != '' && temp.indexOf(h) == -1)
                 temp.push(h);
         }
         if(temp.indexOf(item) == -1)
             temp.push(item);
         return temp.join(',')
-    }
+    };
 
     var hospitalSelection = function() {
         var hospital_id = String($(this).data('hospital-id'));
-        var hospitals = $('#hospitals').val().split(',');
-        $('#hospitals').val(assembleArray(hospital_id, hospitals));
+        var hospitalsElement = $("#hospitals");
+        var hospitals = hospitalsElement.val().split(',');
+        hospitalsElement.val(assembleArray(hospital_id, hospitals));
         updateComparison();
         disableButton($('#hospital_' + hospital_id));
         var hospital_name = String($(this).data('hospital-display'));
-        $('#takeoverItem').html(hospital_name);
-        $('#hintSuccess').prop('hidden', false);
-        $('html, body').animate({
+        $("#takeoverItem").html(hospital_name);
+        $("#hintSuccess").prop('hidden', false);
+        $("html, body").animate({
             scrollTop: $("#hintSuccess").offset().top
         }, 700);
-    }
+    };
 
     var codeSelection = function() {
         var code = String($(this).data('code'));
-        var codes = $('#codes').val().split(',');
-        $('#codes').val(assembleArray(code, codes));
+        var codesElement = $('#codes');
+        var codes = codesElement.val().split(',');
+        codesElement.val(assembleArray(code, codes));
         updateComparison();
         disableButton($('#code_' + code));
         var codeDisplay = String($(this).data('code-display'));
-        $('#takeoverItem').html(codeDisplay);
-        $('#hintSuccess').prop('hidden', false);
-        $('html, body').animate({
+        $("#takeoverItem").html(codeDisplay);
+        $("#hintSuccess").prop('hidden', false);
+        $("html, body").animate({
             scrollTop: $("#hintSuccess").offset().top
         }, 700);
-    }
+    };
 
     var disableAlreadySelected = function(){
-        var codes = $('#codes').val().split(',');
-        var hospitals = $('#hospitals').val().split(',');
+        var codes = $("#codes").val().split(',');
+        var hospitals = $("#hospitals").val().split(',');
         for (var i = 0; i < codes.length; i++) {
-            disableButton($('#code_' + codes[i]));
+            disableButton($("#code_" + codes[i]));
         }
-        for (var i = 0; i < hospitals.length; i++) {
-           disableButton($('#hospital_' + hospitals[i]));
+        for (i = 0; i < hospitals.length; i++) {
+           disableButton($("#hospital_" + hospitals[i]));
         }
-    }
+    } ;
 
     var disableButton = function(entry) {
         entry.addClass('alreadySelected');
         var button =  entry.find( ".btn" );
         button.prop("disabled",true);
         button.prop('title', I18n.t('already_in_selection'));
-    }
+    };
 
     var search = function(e, activeTab) {
         var code = e.which;
         if(code==13) {
-            $('#hospital_search').blur();
-            $('#codes_search').blur();
+            $("#hospital_search").blur();
+            $("#codes_search").blur();
             $(activeTab).addClass('highlightSearchResults');
             $(activeTab).removeClass('highlightSearchResults', 2000);
             $(activeTab).focus();
@@ -194,9 +203,10 @@ $( function() {
             return;
         }
 
-        var searchTermHospital = $('#hospital_search').val();
+        var hospitalSearch = $("#hospital_search");
+        var searchTermHospital = hospitalSearch.val();
         var searchTermCodes = $('#codes_search').val();
-        var searchUrl = $('#hospital_search').data('search-url') + '.html';
+        var searchUrl = hospitalSearch.data('search-url') + '.html';
         $.get(searchUrl, {term_hospitals: searchTermHospital, term_codes: searchTermCodes, limit: 6})
             .done(function (data) {
                 $('#search-results').html(data);
@@ -206,13 +216,16 @@ $( function() {
                 disableAlreadySelected();
                 setURL();
             });
-    }
+    };
 
-    $('#hospital_search').keyup(function(e) {search(e, "#hospitalSearchResults")});
-    $('#codes_search').keyup(function(e) {search(e, "#codeSearchResults")});
+    var hospitalSearch = $("#hospital_search");
+    var codesSearch = $("#codes_search");
 
-    $('#hospital_search').focus(function(e) {search(e, "#hospitalSearchResults")});
-    $('#codes_search').focus(function(e) {search(e, "#codeSearchResults")});
+    hospitalSearch.keyup(function(e) {search(e, "#hospitalSearchResults")});
+    codesSearch.keyup(function(e) {search(e, "#codeSearchResults")});
+
+    hospitalSearch.focus(function(e) {search(e, "#hospitalSearchResults")});
+    codesSearch.focus(function(e) {search(e, "#codeSearchResults")});
 
 
     addRemoval();
@@ -223,15 +236,15 @@ $( function() {
     $('#resultsTabs li a').on('shown.bs.tab', function (e) { setURL() });
 
     // trigger a hospital search if the search field for hospitals has an initial value on page load.
-    if($('#hospital_search').val() != '')
-        $('#hospital_search').trigger( "keyup" );
+    if(hospitalSearch.val() != '')
+        hospitalSearch.trigger( "keyup" );
 
-    $('#deleteAllButton').on('click', function(e) {
-        $('#hospitals').val('');
-        $('#codes').val('');
-        $('#hospital_search').val('');
-        $('#codes_search').val('');
-        search(e, '#hospitalSearchResults')
+    $("#deleteAllButton").on('click', function(e) {
+        $("#hospitals").val('');
+        $("#codes").val('');
+        $("#hospital_search").val('');
+        $("#codes_search").val('');
+        search(e, '#hospitalSearchResults');
         updateComparison();
     })
 });
--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -9,6 +9,6 @@ class CodesController < ApplicationController
     @code_class = {Mdc: Mdc, Partition: Partition, Adrg: Adrg, Drg: Drg}[params[:level].to_sym]
     @code = @code_class.find(params[:code])
 
-    @num_cases = NumCase.where(version: @system.version, year: @system.base_year, code: @code.code, level: params[:level].upcase).order('n desc')
+    @num_cases = NumCase.where(version: @system.version, year: @system.base_year, code: @code.code, level: params[:level].upcase).where('n > 0').order('n desc')
   end
 end
