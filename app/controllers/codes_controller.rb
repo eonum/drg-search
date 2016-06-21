cb6d125e22f115ffdfc94e697dddfe21574023b1
Tim Peter <tim.peter@eonum.ch>
2016-06-21 11:29:51 +0200

    Get hospital and code from params.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -2,6 +2,8 @@ class CodesController < ApplicationController
   include CodeHelper
 
   def show
-
+    @system = System.find(params[:system_id])
+    @code_class = {Mdc: Mdc, Partition: Partition, Adrg: Adrg, Drg: Drg}[params[:level].to_sym]
+    @code = @code_class.find(params[:code])
   end
 end
--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -2,6 +2,7 @@ class HospitalsController < ApplicationController
   include HospitalHelper
 
   def show
-
+    @system = System.find(params[:system_id])
+    @hospital = Hospital.find(params[:hospital])
   end
 end
