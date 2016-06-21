49da5b1a4636f3def03b095ec4242ffb75c6feb3
Tim Peter <tim.peter@eonum.ch>
2016-06-21 20:01:32 +0200

    Display all services delivered by a hospital.

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -4,5 +4,7 @@ class HospitalsController < ApplicationController
   def show
     @system = System.find(params[:system_id])
     @hospital = Hospital.find(params[:hospital])
+
+    @num_cases = NumCase.where(version: @system.version, year: @system.base_year, hospital_id: @hospital.hospital_id, level: params[:level].upcase).where('n > 0').order('n desc')
   end
 end
--- app/models/num_case.rb
+++ app/models/num_case.rb
@@ -4,4 +4,9 @@ class NumCase < ActiveRecord::Base
   def hospital
     return Hospital.where(hospital_id: self.hospital_id, year: self.year).first
   end
+
+  def code_link
+    code_class = {'MDC': Mdc, 'PARTITION': Partition, 'ADRG': Adrg, 'DRG': Drg}[self.level.to_sym]
+    return code_class.where(code: self.code, version: self.version).first
+  end
 end
