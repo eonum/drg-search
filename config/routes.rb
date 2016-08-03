55aaffd7a4d4b84160913f95484fed37e913d38b
Tim Peter <tim.peter@eonum.ch>
2016-08-03 17:57:10 +0200

    Improve handling of EN translations.
    Everything on the starting page in EN is in English now. Everything else will redirect to the German version.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: 'home#redirect'
 
+  get '/en/systems/*path', to: redirect("/de/systems/%{path}")
+
   scope '/:locale', :locale => /de|fr|it|en/, :format => /json|html/ do
     get '', to: 'home#index', as: 'home'
     get 'contact', to: 'home#contact'
     get 'documentation', to: 'home#documentation'
-
 
     resources :systems,  only: ['index', 'show'] do
       get :compare, on: :member
