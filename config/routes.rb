b339b754d18bd0ee65f34b598901ac21b09c3d53
Ramona <ramona.beck@students.unibe.ch>
2018-08-13 16:07:55 +0200

    Update rails app using 'rails app:update'

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#redirect'
-
+  
   # redirect all English requests except the landing page to its German equivalents
   get '/en/systems/*path', to: redirect("/de/systems/%{path}")
-
+  
   scope '/:locale', :locale => /de|fr|it|en/, :format => /json|html/ do
     get '', to: 'home#index', as: 'home'
     get 'contact', to: 'home#contact'
     get 'documentation', to: 'home#documentation'
-
+  
     resources :systems,  only: ['index', 'show'] do
       get :compare, on: :member
       get 'search/all'
       resources :hospitals, only: [:show]
       resources :codes, only: [:show]
     end
-  end
+end
+
 end
