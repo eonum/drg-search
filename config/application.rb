3b5e1ed5daa880e6783a2864d328d5e09f2605f0
Ramona <ramona.beck@students.unibe.ch>
2018-08-24 11:52:01 +0200

    Update config files from rails 5.0 to rails 5.1 by running 'rails app:update'

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module DrgSearch
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
