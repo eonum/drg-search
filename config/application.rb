8303fb919d5ddec057578ac6307f5f7d7ef68565
Ramona <ramona.beck@students.unibe.ch>
2018-08-28 15:34:10 +0200

    Add i18n settings

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module DrgSearch
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:de, :fr, :it, :en]
+    config.i18n.default_locale = :de
   end
 end
